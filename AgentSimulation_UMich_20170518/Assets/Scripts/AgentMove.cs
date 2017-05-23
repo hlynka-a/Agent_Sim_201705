4eaa7d5f9490defae3e1aa423d93da1fbc0f8a66
hlynka-a <hlynka1@hotmail.com>
2017-05-23 01:10:26 -0400

    2nd Push - Improved AI, documentation
    Agent AI improved, less likely to get stuck in part of complex maze.
    Code documentation for agent AI improved.
    Agent interaction still TODO.

--- AgentSimulation_UMich_20170518/Assets/Scripts/AgentMove.cs
+++ AgentSimulation_UMich_20170518/Assets/Scripts/AgentMove.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/* 
+AgentMove.cs
+- Controls basic AI system for individual agent.
+*/
+
+
 public class AgentMove : MonoBehaviour {
 
 	// goal
@@ -89,24 +95,31 @@ public class AgentMove : MonoBehaviour {
 			MoveForward ();
 			break;
 		case 3:
-			//TODO
 			FindNewTarget();
 			break;
 		case 4:
 			RotateToTarget ();
+			break;
+		case 5: 
+			StartSearchingNewTarget ();
 			break;
 		default:
 			break;
 		}
 	}
 
+	// targetRotaiton/previousRotation = used to have consistent rotation speed between start and end rotation
 	Quaternion targetRotation = Quaternion.identity;
 	Quaternion previousRotation = Quaternion.identity;
 	float timePassed = 0f;
 	public float speedRotation = 1f;
 	public float speedMove = 1f;
 
+	/* RotateToTarget() :
+	 - rotation angle already determined - rotate towards it at consistent speed.
+	*/
 	void RotateToTarget(){
+		// if rotation angle hasn't actually been set yet, calculate based on target position (target is not necessarily final goal)
 		if (targetRotation == null || targetRotation == Quaternion.identity) {
 			targetRotation = Quaternion.LookRotation (target - this.transform.position);
 			previousRotation = this.transform.rotation;
@@ -116,25 +129,60 @@ public class AgentMove : MonoBehaviour {
 		timePassed += Time.deltaTime;
 
 		float rotateMagnitude = timePassed * speedRotation;
-
 		// rotate towards target, also restrict rotation not to be above
 		this.transform.rotation = Quaternion.Lerp (previousRotation, targetRotation, rotateMagnitude);
 		this.transform.eulerAngles = new Vector3 (0,this.transform.rotation.eulerAngles.y, 0);
 
+		// Quaternion.Lerp(a,b,1) => a, Quaternion.Lerp(a,b,1) => b, use this to determine if finished. 
 		if (rotateMagnitude >= 1.0f) {
 			state = 2;
 			targetRotation = Quaternion.identity;
 		}
 	}
 
+	// angleToRotate = increment to see how many angles to rotate
 	int angleToRotate = 0;
+	// leftPosition/rightPosition = target position to look at rotated left/right, to see if obstacles in between
 	Vector3 leftPosition = Vector3.zero;
 	Vector3 rightPosition = Vector3.zero;
-	void FindNewTarget(){
+	// targetDirectionCounter/targetDirectionMod = to determine if to reset rotation towards final goal or not
+	int targetDirectionCounter = 0;
+	int targetDirectionMod = 3;
+	// target to reset to before rotating to find best direction
+	Vector3 originalTarget = Vector3.zero;
+
+	/* StartSearchingNewTarget():
+	- Decide where to initialize agent rotation to determine best next direction to move.
+	- By sometimes initializing towards final goal, helps reset agent to eventually make it towards end. 
+	*/
+	void StartSearchingNewTarget(){
 		// start with goal, then rotate left and right to decide which will allow to move forward
 
-		Vector3 originalTarget = goal.position;
+		// A = nearest rotation from current rotation, B = neareest rotation from rotation to goal
+		// 1 => A, 2 => A,  3 => B, 
+		// 4 => A, 5 => A,  6 => A,  7 => B, 
+		// 8 => A, 9 => A, 10 => A, 11 => A, 12 => B, ...
+		targetDirectionCounter++;
+		if (targetDirectionCounter % targetDirectionMod == 0) {
+			originalTarget = goal.position;
+			targetDirectionCounter++;
+			targetDirectionMod++;
+			if (targetDirectionMod > 7) {
+				targetDirectionMod = 3;
+			}
+		} else {
+			originalTarget = this.transform.position + (this.transform.forward*10);
+		}
 
+		state = 3;
+	}
+
+	/* FindNewTarget():
+	- Find least amount to rotate to be able to move (left or right)
+	*/
+	void FindNewTarget(){
+
+		// note: set to update search angle every frame, to easily trace.
 		while (angleToRotate < 180){
 			RaycastHit hitInfo;
 			leftPosition = (Quaternion.Euler (new Vector3(0, -angleToRotate, 0)) * (originalTarget - this.transform.position)) 
@@ -143,8 +191,6 @@ public class AgentMove : MonoBehaviour {
 			    + this.transform.position;
 			if (!Physics.Raycast(this.transform.position, 
 				(leftPosition - this.transform.position), out hitInfo, maxDistanceBlock * 2)){
-			/*if (!Physics.SphereCast (this.transform.position, 1, 
-				(leftPosition - this.transform.position), out hitInfo, maxDistanceBlock * 2)) {*/
 				// leftPosition is good! Move towards it in next step.
 				target = leftPosition;
 				angleToRotate = 0;
@@ -164,31 +210,33 @@ public class AgentMove : MonoBehaviour {
 				// to see angle searching in real time
 				angleToRotate++;
 				target = leftPosition;
+				// to calculate immediately (not real time), remove break; line here.
 				break;
 			}
 
 		}
-
 		//assumption: agent will not be trapped - they can at the very least move backwards.
-
 	}
 
-	// how far to look ahead before looking away?
+	// how far to look ahead before looking for a way around?
 	float maxDistanceBlock = 0.5f;
 
+	/* MoveForward():
+	- agent should be rotated in correct direction, this function is to move forward.
+	*/
 	void MoveForward(){
-
 		//what if something is in the way?
 		RaycastHit hitInfo;
 		if (Physics.Raycast (this.transform.position, this.transform.forward, out hitInfo, maxDistanceBlock)) {
 			// you are about to hit something! But what if it is a player vs a wall?
 			// for now, just focus on wall logic
-			state = 3;
+			state = 5;
 		} else {
 			// what if there is no direct obstacle between you and the goal? What if you are not already moving towards the goal? You might move past it!
 			// ignore other agents for this... they are set to be on layer 8
 			LayerMask ignoreAgents = 0;
 			ignoreAgents = 1 << 8;
+			ignoreAgents = ~ignoreAgents;
 			if (target != goal.transform.position
 			    && !Physics.Raycast (this.transform.position, 
 						goal.position - this.transform.position, 
@@ -198,9 +246,9 @@ public class AgentMove : MonoBehaviour {
 				this.transform.eulerAngles = new Vector3 (0,this.transform.rotation.eulerAngles.y, 0);
 			} 
 
+			// move forward
 			this.transform.Translate (Vector3.forward * speedMove * Time.deltaTime);
-
-			//what if I'm too big and about to graze something?
+			//what if I'm too big and about to graze something? Strafe to the left or right a bit...
 			if (Physics.Raycast (this.transform.position + (this.transform.right * this.transform.lossyScale.x * 0.5f), this.transform.forward, out hitInfo, maxDistanceBlock)) {
 				this.transform.Translate (-Vector3.right * speedMove * Time.deltaTime);
 			} 
@@ -212,6 +260,9 @@ public class AgentMove : MonoBehaviour {
 	}
 
 
+	/* ShowDebugRay():
+	- optional function to show ray lines while executed in Unity editor to help trace AI.
+	*/
 	void ShowDebugRay(){
 		Debug.DrawRay (this.transform.position, 
 			(goal.position - this.transform.position).normalized * (Vector3.Distance(this.transform.position,goal.position)), 
